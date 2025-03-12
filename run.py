42b35a3308876b72828ec8e37bc00c469c723a4c
Wingman2025 <jorgescm86@gmail.com>
2025-03-12 23:38:02 +0100

    railway2

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5010)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Railway asigna
+    app.run(debug=False, host='0.0.0.0', port=port)
+
